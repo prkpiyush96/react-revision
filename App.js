a5da91b4e1199e654b522abe056407bedcd633a1
Piyush Personal <prkpiyush96@gmail.com>
2023-11-07 23:09:24 +0530

    food order app with dummy data - init

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { key: "h1" }, "I am an h1 tag"),
-    React.createElement("h2", { key: "h2" }, "I am an h2 tag"),
-  ])
-);
+const resList = require("./dummuResData.json").restaurants;
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World!")
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          alt="logo"
+          src="https://dynamic.brandcrowd.com/asset/logo/7f982a19-779d-4dd3-b533-7a9f66474000/logo-search-grid-1x?logoTemplateVersion=1&v=637810055012670000"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Anout Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-console.log(parent, "heading")
+const RestaurantCard = ({ resData }) => {
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
+    resData?.info;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating + " Stars"}</h4>
+      <h4>{costForTwo}</h4>
+    </div>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resList?.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-root.render(parent)
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
