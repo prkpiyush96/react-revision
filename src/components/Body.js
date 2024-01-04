d91370a2f26f3537f44a862f11ed880b0c46c4f6
Piyush Personal <prkpiyush96@gmail.com>
2024-01-04 23:10:37 +0530

    add delay to mock API behavior, shimmer loader

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "./mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [restaurantsList, setRestaurantsList] = useState(restaurants);
-  console.log(restaurantsList);
+  const [restaurantsList, setRestaurantsList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    // const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9530189&lng=77.7088108');
+    // const res = await data.json();
+    
+    setTimeout(() => {
+      setRestaurantsList(restaurants);
+    }, 1500);
+  }
+
+  if (restaurantsList?.length === 0) {
+    return <Shimmer />
+  }
+
   return (
     <div className="body">
       <div className="filter">
---
+++ src/components/Shimmer.js
@@ -0,0 +1,19 @@
+const Shimmer = () => {
+    return (
+        <div className="shimmer-container">
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+            <div className="shimmer-card" />
+        </div>
+    )
+}
+
+export default Shimmer;
