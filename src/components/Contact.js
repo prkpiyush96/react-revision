1827b939adb7eb35d4ac960b73fb2ffd7297ff54
Piyush Personal <prkpiyush96@gmail.com>
2024-01-16 20:27:01 +0530

    basic routing

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>CONTACT US</h1>
+            <h2>Piyush Pareek</h2>
+        </div>
+    )
+}
+
+export default Contact
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
@@ -11,10 +12,18 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>Anout Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About Us</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+          <li>
+            <Link to={"/"}>Cart</Link>
+          </li>
           <button
             className="login"
             onClick={() => {
